ed9095500ea358e4ab2e0a402e7d4c6be5a2bed8
Nguyễn Thành Lộc <nguyenthanhloc1308@gmail.com>
2021-06-27 21:55:07 +0700

    Final

--- shared/loginUrl.js
+++ shared/loginUrl.js
@@ -2,4 +2,5 @@ import Constants from "expo-constants";
 
 const { manifest } = Constants;
 
-export const loginUrl = `http://${manifest.debuggerHost.split(':').shift()}:5000/api/users/`;
+//export const loginUrl = `http://${manifest.debuggerHost.split(':').shift()}:5000/api/users/`;
+export const loginUrl = `http://tranquil-dawn-06675.herokuapp.com/api/users/`;
--- shared/reserveUrl.js
+++ shared/reserveUrl.js
@@ -2,4 +2,6 @@ import Constants from "expo-constants";
 
 const { manifest } = Constants;
 
-export const reserveUrl = `http://${manifest.debuggerHost.split(':').shift()}:5000/api/reserve/`;
+//export const reserveUrl = `http://${manifest.debuggerHost.split(':').shift()}:5000/api/reserve/`;
+
+export const reserveUrl = `http://tranquil-dawn-06675.herokuapp.com/api/reserve/`;
